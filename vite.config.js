4ddff0de9ed0f61de1523085bd9f16331c0ac90c
Lasse Boisen Andersen <la@laander.com>
2022-03-08 17:23:25 +0100

    rename, cleanup

--- vite.config.js
+++ vite.config.js
@@ -11,22 +11,8 @@ module.exports = defineConfig({
         `bucket-tracking.${format}.${format === "es" ? "m" : ""}js`,
     },
     rollupOptions: {
-      // make sure to externalize deps that shouldn't be bundled
-      // into your library
-      // external: ["vue"],
-      output: {
-        // Provide global variables to use in the UMD build
-        // for externalized deps
-        // globals: {
-        //   vue: "Vue",
-        // },
-      },
+      output: {},
     },
   },
-  plugins: [
-    dts({
-      // insertTypesEntry: true,
-      // staticImport: true,
-    }),
-  ],
+  plugins: [dts()],
 });
