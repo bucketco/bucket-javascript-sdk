749ba344936a2b3a05ba0766943ae0f9c262351e
Richard Foster <richard@rpf.me>
2023-08-25 14:07:10 +0100

    Add acceptance test using built Node.js SDK

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,13 @@ import { defineConfig } from "vite";
 export default defineConfig({
   test: {
     environment: "jsdom",
+    exclude: [
+      "test/e2e/**",
+      "**/node_modules/**",
+      "**/dist/**",
+      "**/cypress/**",
+      "**/.{idea,git,cache,output,temp}/**",
+      "**/{karma,rollup,webpack,vite,vitest,jest,ava,babel,nyc,cypress,tsup,build}.config.*",
+    ],
   },
 });
