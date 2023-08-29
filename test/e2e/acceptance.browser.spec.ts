c6a0f8d3051477c4f81f3e6c8e588502b34d57a3
Richard Foster <richard@rpf.me>
2023-08-29 12:17:32 +0100

    Add E2E tests around built SDK (#32)
    * Add acceptance test using built Node.js SDK
    
    * Add acceptance test using built browser SDK
    
    * Update test commands for E2E tests
    
    * Force a build before running e2e tests locally
    
    * Install playwright browsers in CI
    
    * Add vite.config.js to preversion
    
    * Assert all mocks have been used
    
    * Add test reporting back
    
    * Fix linting setup problem
    
    * Prettier
    
    ---------
    
    Co-authored-by: Peter Brandt Müller <peter@bucket.co>

---
+++ test/e2e/acceptance.browser.spec.ts
@@ -0,0 +1,96 @@
+import { randomUUID } from "crypto";
+import { expect, test } from "@playwright/test";
+
+const KEY = randomUUID();
+
+test("Acceptance", async ({ page }) => {
+  const successfulRequests: string[] = [];
+
+  // Mock API calls with assertions
+  await page.route(`https://tracking.bucket.co/${KEY}/user`, async (route) => {
+    expect(route.request().method()).toEqual("POST");
+    expect(route.request().postDataJSON()).toMatchObject({
+      userId: "foo",
+      attributes: {
+        name: "john doe",
+      },
+    });
+
+    successfulRequests.push("USER");
+    await route.fulfill({ status: 200 });
+  });
+
+  await page.route(
+    `https://tracking.bucket.co/${KEY}/company`,
+    async (route) => {
+      expect(route.request().method()).toEqual("POST");
+      expect(route.request().postDataJSON()).toMatchObject({
+        userId: "foo",
+        companyId: "bar",
+        attributes: {
+          name: "bar corp",
+        },
+      });
+
+      successfulRequests.push("COMPANY");
+      await route.fulfill({ status: 200 });
+    },
+  );
+
+  await page.route(`https://tracking.bucket.co/${KEY}/event`, async (route) => {
+    expect(route.request().method()).toEqual("POST");
+    expect(route.request().postDataJSON()).toMatchObject({
+      userId: "foo",
+      companyId: "bar",
+      event: "baz",
+      attributes: {
+        baz: true,
+      },
+    });
+
+    successfulRequests.push("EVENT");
+    await route.fulfill({ status: 200 });
+  });
+
+  await page.route(
+    `https://tracking.bucket.co/${KEY}/feedback`,
+    async (route) => {
+      expect(route.request().method()).toEqual("POST");
+      expect(route.request().postDataJSON()).toMatchObject({
+        userId: "foo",
+        companyId: "bar",
+        featureId: "featureId1",
+        score: 5,
+        comment: "test!",
+      });
+
+      successfulRequests.push("FEEDBACK");
+      await route.fulfill({ status: 200 });
+    },
+  );
+
+  // Load the built Bucket SDK
+  await page.addScriptTag({
+    url: "http://localhost:8000/dist/bucket-tracking-sdk.browser.js",
+  });
+
+  // Golden path requests
+  await page.evaluate(`
+    ;(async () => {
+      bucket.init("${KEY}", {});
+      await bucket.user("foo", { name: "john doe" });
+      await bucket.company("bar", { name: "bar corp" }, "foo");
+      await bucket.track("baz", { baz: true }, "foo", "bar");
+      await bucket.feedback({
+        featureId: "featureId1",
+        userId: "foo",
+        companyId: "bar",
+        score: 5,
+        comment: "test!",
+      });
+    })()
+  `);
+
+  // Assert all API requests were made
+  expect(successfulRequests).toEqual(["USER", "COMPANY", "EVENT", "FEEDBACK"]);
+});
---
+++ test/e2e/acceptance.node.test.ts
@@ -0,0 +1,63 @@
+// @vitest-environment node
+import { randomUUID } from "crypto";
+import nock from "nock";
+import { beforeEach, expect, test } from "vitest";
+
+import bucket from "../../";
+
+const KEY = randomUUID();
+
+beforeEach(() => nock.cleanAll());
+
+test("Acceptance", async () => {
+  nock(`https://tracking.bucket.co/${KEY}`)
+    .post(/.*\/user/, {
+      userId: "foo",
+      attributes: {
+        name: "john doe",
+      },
+    })
+    .reply(200);
+  nock(`https://tracking.bucket.co/${KEY}`)
+    .post(/.*\/company/, {
+      userId: "foo",
+      companyId: "bar",
+      attributes: {
+        name: "bar corp",
+      },
+    })
+    .reply(200);
+  nock(`https://tracking.bucket.co/${KEY}`)
+    .post(/.*\/event/, {
+      userId: "foo",
+      companyId: "bar",
+      event: "baz",
+      attributes: {
+        baz: true,
+      },
+    })
+    .reply(200);
+  nock(`https://tracking.bucket.co/${KEY}`)
+    .post(/.*\/feedback/, {
+      userId: "foo",
+      companyId: "bar",
+      featureId: "featureId1",
+      score: 5,
+      comment: "test!",
+    })
+    .reply(200);
+
+  bucket.init(KEY, {});
+  await bucket.user("foo", { name: "john doe" });
+  await bucket.company("bar", { name: "bar corp" }, "foo");
+  await bucket.track("baz", { baz: true }, "foo", "bar");
+  await bucket.feedback({
+    featureId: "featureId1",
+    userId: "foo",
+    companyId: "bar",
+    score: 5,
+    comment: "test!",
+  });
+
+  expect(nock.isDone()).toBeTruthy();
+});
