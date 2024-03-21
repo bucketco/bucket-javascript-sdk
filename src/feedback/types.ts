3957cc61bf2681456617b810c29ef25f2b56a0a2
Richard Foster <richard@rpf.me>
2024-03-21 12:51:54 +0000

    Document x and y offset options
    Co-authored-by: Peter Müller <peter@bucket.co>

--- src/feedback/types.ts
+++ src/feedback/types.ts
@@ -7,7 +7,13 @@ export type FeedbackPlacement =
   | "top-left";
 
 export type Offset = {
+  /**
+   * Offset from the nearest horizontal screen edge after placement is resolved
+   */
   x?: string | number;
+  /**
+   * Offset from the nearest vertical screen edge after placement is resolved
+   */
   y?: string | number;
 };
 
