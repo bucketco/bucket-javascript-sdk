b077f78674d51eebdad4e4cb6021e2f11d516e1f
Peter Brandt Müller <peter@bucket.co>
2024-03-23 12:14:05 +0100

    Remove accidental duplicated test

--- test/usage.test.ts
+++ test/usage.test.ts
@@ -337,26 +337,6 @@ describe("usage", () => {
         .resolves;
     });
   });
-
-  test("can reset user", async () => {
-    nock(`${TRACKING_HOST}/${KEY}`)
-      .post(/.*\/user/, {
-        userId: "foo",
-        attributes: {
-          name: "john doe",
-        },
-      })
-      .reply(200);
-
-    const bucketInstance = bucket();
-    bucketInstance.init(KEY, { persistUser: true });
-    await bucketInstance.user("foo", { name: "john doe" });
-
-    bucketInstance.reset();
-    await expect(() => bucketInstance.track("foo")).rejects.toThrowError(
-      "User is not set, please call user() first",
-    );
-  });
 });
 
 describe("feedback prompting", () => {
