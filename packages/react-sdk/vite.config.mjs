ce41d6f6fad7dbfdb368dd7b7347b75770a54961
Ron Cohen <ron@bucket.co>
2024-07-31 11:11:28 +0200

    cleanup

--- packages/react-sdk/vite.config.mjs
+++ packages/react-sdk/vite.config.mjs
@@ -14,25 +14,13 @@ export default defineConfig({
     exclude: ["**/node_modules/**", "test/e2e/**"],
     sourcemap: true,
     lib: {
-      // Could also be a dictionary or array of multiple entry points
       entry: resolve(__dirname, "src/index.tsx"),
       name: "BucketReactSDK",
-      // the proper extensions will be added
       fileName: "bucket-react-sdk",
       formats: ["es"],
     },
     rollupOptions: {
-      //   // make sure to externalize deps that shouldn't be bundled
-      //   // into your library
-
       external: ["react", "react-dom"],
-      //   output: {
-      //     // Provide global variables to use in the UMD build
-      //     // for externalized deps
-      //     globals: {
-      //       BucketClient: "BucketClient",
-      //     },
-      //   },
     },
   },
 });
