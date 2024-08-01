eafd183d7d96054e5ccac223aaf1ee779cc44bbd
Ron Cohen <ron@bucket.co>
2024-08-01 09:02:00 +0200

    refactor flags context

--- packages/react-sdk/src/index.tsx
+++ packages/react-sdk/src/index.tsx
@@ -84,6 +84,7 @@ export function BucketProvider({
   ...config
 }: BucketProps) {
   const [flagsLoading, setFlagsLoading] = useState(true);
+  const [flags, setFlags] = useState<FlagsResult>({});
   const ref = useRef<BucketClient>();
 
   const [flagContext, setFlagContext] = useState({
@@ -114,6 +115,7 @@ export function BucketProvider({
     client
       .initialize()
       .then(() => {
+        setFlags(client.getFlags() ?? {});
         setFlagsLoading(false);
 
         // update user attributes
@@ -199,9 +201,10 @@ export function BucketProvider({
     },
     [user?.id, company?.id],
   );
+
   const context: ProviderContextType = {
     flags: {
-      flags: ref.current?.getFlags() ?? {},
+      flags,
       isLoading: flagsLoading,
     },
     updateUser,
@@ -246,10 +249,12 @@ export function useFlagIsEnabled(flagKey: BucketFlags) {
  * ```
  */
 export function useFlag(key: BucketFlags) {
-  const flags = useContext<ProviderContextType>(ProviderContext).flags;
-  const isEnabled = flags.flags[key] ?? false;
+  const { flags, isLoading } =
+    useContext<ProviderContextType>(ProviderContext).flags;
 
-  return { isLoading: flags.isLoading, isEnabled };
+  const isEnabled = flags[key] ?? false;
+
+  return { isLoading: isLoading, isEnabled };
 }
 
 /**
@@ -272,9 +277,8 @@ export function useFlags(): {
   isLoading: boolean;
   flags: FlagsResult;
 } {
-  const {
-    flags: { flags, isLoading },
-  } = useContext<ProviderContextType>(ProviderContext);
+  const { flags, isLoading } =
+    useContext<ProviderContextType>(ProviderContext).flags;
 
   return {
     isLoading,
