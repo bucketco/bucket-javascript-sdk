79a9757a66496328488b2d4908bb9b65c5b07838
Alexandru Ciobanu <alex+git@ciobanu.org>
2025-02-20 20:02:39 +0700

    feat(openfeature-node-provider): enhance todo feature management with config and validation (#330)

--- packages/openfeature-node-provider/example/bucket.ts
+++ packages/openfeature-node-provider/example/bucket.ts
@@ -5,11 +5,29 @@ if (!process.env.BUCKET_SECRET_KEY) {
   throw new Error("BUCKET_SECRET_KEY is required");
 }
 
+export type CreateTodosConfig = {
+  maxLength: number;
+};
+
 const provider = new BucketNodeProvider({
   secretKey: process.env.BUCKET_SECRET_KEY!,
-  fallbackFeatures: ["show-todos"],
+  fallbackFeatures: {
+    "show-todos": {
+      isEnabled: true,
+    },
+    "create-todos": {
+      isEnabled: true,
+      config: {
+        key: "default",
+        payload: {
+          maxLength: 100,
+        },
+      },
+    },
+  },
   logger: console,
 });
+
 OpenFeature.setProvider(provider);
 
 export default provider;
