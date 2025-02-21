2ae2c2fdeb9890e24b864cff7a787c86b8b7dbe6
Lasse Boisen Andersen <la@laander.com>
2025-02-21 14:15:15 +0100

    staggered anim on feature rows on open

--- packages/browser-sdk/src/toolbar/Toolbar.tsx
+++ packages/browser-sdk/src/toolbar/Toolbar.tsx
@@ -105,6 +105,7 @@ export default function Toolbar({
               bucketClient,
             )}
             appBaseUrl={appBaseUrl}
+            isOpen={isOpen}
           />
         </DialogContent>
       </Dialog>
