26962c6cfdbe742069c0e0377efacd5dea7074d0
Erik Hughes <erik.hughes@outlook.com>
2025-02-26 12:14:20 +0100

    refactor(tracking-sdk): official deprecate

--- packages/tracking-sdk/src/config.ts
+++
@@ -1,8 +0,0 @@
-import { version } from "../package.json";
-
-export const API_HOST = "https://tracking.bucket.co";
-export const SSE_REALTIME_HOST = "https://livemessaging.bucket.co";
-
-export const SDK_VERSION_HEADER_NAME = "bucket-sdk-version";
-
-export const SDK_VERSION = version;
--- packages/tracking-sdk/src/feedback/packages/floating-ui-preact-dom/utils/useLatestRef.ts
+++
@@ -1,9 +0,0 @@
-import { useLayoutEffect, useRef } from "preact/hooks";
-
-export function useLatestRef<T>(value: T) {
-  const ref = useRef(value);
-  useLayoutEffect(() => {
-    ref.current = value;
-  });
-  return ref;
-}
