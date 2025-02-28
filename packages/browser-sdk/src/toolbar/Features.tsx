aeb3bc6eb0be783263f97905b83ec3d6aec2e11c
Lasse Boisen Andersen <lasse@bucket.co>
2025-02-28 15:14:08 +0100

    Toolbar: Improve serach result and keyboard navigation (#352)
    Less jumpy searching where where the whole dialog doesn't resize:
      
    
    https://github.com/user-attachments/assets/6765d174-44f9-4328-8330-832606c0c675
    
    Improve keyboard accessibility:
      
    
    https://github.com/user-attachments/assets/9019f72d-1f05-4d9a-bd56-a96f8912ee76

--- packages/browser-sdk/src/toolbar/Features.tsx
+++ packages/browser-sdk/src/toolbar/Features.tsx
@@ -6,27 +6,40 @@ import { FeatureItem } from "./Toolbar";
 
 export function FeaturesTable({
   features,
+  searchQuery,
   setEnabledOverride,
   appBaseUrl,
   isOpen,
 }: {
   features: FeatureItem[];
+  searchQuery: string | null;
   setEnabledOverride: (key: string, value: boolean | null) => void;
   appBaseUrl: string;
   isOpen: boolean;
 }) {
-  if (features.length === 0) {
+  const searchedFeatures =
+    searchQuery === null
+      ? features
+      : [...features].sort((a, _b) => (a.key.includes(searchQuery) ? -1 : 1));
+
+  if (searchedFeatures.length === 0) {
     return <div style={{ color: "var(--gray500)" }}>No features found</div>;
   }
   return (
-    <table class="features-table" style={{ "--n": features.length }}>
+    <table class="features-table" style={{ "--n": searchedFeatures.length }}>
       <tbody>
-        {features.map((feature, index) => (
+        {searchedFeatures.map((feature, index) => (
           <FeatureRow
             key={feature.key}
             appBaseUrl={appBaseUrl}
             feature={feature}
             index={index}
+            isNotVisible={
+              searchQuery !== null &&
+              !feature.key
+                .toLocaleLowerCase()
+                .includes(searchQuery.toLocaleLowerCase())
+            }
             isOpen={isOpen}
             setEnabledOverride={setEnabledOverride}
           />
@@ -42,21 +55,27 @@ function FeatureRow({
   feature,
   isOpen,
   index,
+  isNotVisible,
 }: {
   feature: FeatureItem;
   appBaseUrl: string;
   setEnabledOverride: (key: string, value: boolean | null) => void;
   isOpen: boolean;
   index: number;
+  isNotVisible: boolean;
 }) {
-  const [show, setShow] = useState(true);
+  const [showOnOpen, setShowOnOpen] = useState(isOpen);
   useEffect(() => {
-    setShow(isOpen);
+    setShowOnOpen(isOpen);
   }, [isOpen]);
   return (
     <tr
       key={feature.key}
-      class={["feature-row", show ? "show" : undefined].join(" ")}
+      class={[
+        "feature-row",
+        showOnOpen ? "show-on-open" : undefined,
+        isNotVisible ? "not-visible" : undefined,
+      ].join(" ")}
       style={{ "--i": index }}
     >
       <td class="feature-name-cell">
@@ -64,6 +83,7 @@ function FeatureRow({
           class="feature-link"
           href={`${appBaseUrl}/envs/current/features/by-key/${feature.key}`}
           rel="noreferrer"
+          tabIndex={index + 1}
           target="_blank"
         >
           {feature.key}
@@ -74,6 +94,7 @@ function FeatureRow({
           <Reset
             featureKey={feature.key}
             setEnabledOverride={setEnabledOverride}
+            tabIndex={index + 1}
           />
         ) : null}
       </td>
@@ -83,6 +104,7 @@ function FeatureRow({
             (feature.localOverride === null && feature.isEnabled) ||
             feature.localOverride === true
           }
+          tabIndex={index + 1}
           onChange={(e) => {
             const isChecked = e.currentTarget.checked;
             const isOverridden = isChecked !== feature.isEnabled;
@@ -103,6 +125,7 @@ export function FeatureSearch({
     <input
       class="search-input"
       placeholder="Search features"
+      tabIndex={0}
       type="search"
       autoFocus
       onInput={(s) => onSearch(s.currentTarget.value)}
@@ -113,10 +136,11 @@ export function FeatureSearch({
 function Reset({
   setEnabledOverride,
   featureKey,
+  ...props
 }: {
   setEnabledOverride: (key: string, value: boolean | null) => void;
   featureKey: string;
-}) {
+} & h.JSX.HTMLAttributes<HTMLAnchorElement>) {
   return (
     <a
       class="reset"
@@ -125,6 +149,7 @@ function Reset({
         e.preventDefault();
         setEnabledOverride(featureKey, null);
       }}
+      {...props}
     >
       reset
     </a>
