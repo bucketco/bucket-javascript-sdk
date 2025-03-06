ae67948f1d3f7a98c079e73f350b618661904eca
Matus Vacula <vacula.matus@gmail.com>
2025-03-06 10:51:55 +0100

    chore: fail on docs warnings and export missing types

--- packages/browser-sdk/src/index.ts
+++ packages/browser-sdk/src/index.ts
@@ -1,9 +1,22 @@
-export type { Feature, InitOptions, ToolbarOptions } from "./client";
-export { BucketClient } from "./client";
+export type {
+  Feature,
+  FeatureRemoteConfig,
+  InitOptions,
+  ToolbarOptions,
+} from "./client";
+export { BucketClient, Config } from "./client";
 export type { BucketContext, CompanyContext, UserContext } from "./context";
+export type {
+  Position,
+  Offset,
+  DialogPlacement,
+  PopoverPlacement,
+  ToolbarPosition,
+} from "./ui/types";
 export type {
   CheckEvent,
   FallbackFeatureOverride,
+  FetchedFeature,
   RawFeature,
   RawFeatures,
 } from "./feature/features";
@@ -28,6 +41,6 @@ export type {
   OnScoreSubmitResult,
   OpenFeedbackFormOptions,
 } from "./feedback/ui/types";
-export type { TrackEvent } from "./hooksManager";
+export type { HookArgs, TrackEvent } from "./hooksManager";
 export type { Logger } from "./logger";
 export { feedbackContainerId, propagatedEvents } from "./ui/constants";
