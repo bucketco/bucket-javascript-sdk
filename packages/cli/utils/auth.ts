2c265e6bbcff13eb6210e329174b7cd1521bd64c
Erik Hughes <erik.hughes@outlook.com>
2025-04-29 21:55:20 +0200

    style: fix order

--- packages/cli/utils/auth.ts
+++ packages/cli/utils/auth.ts
@@ -6,8 +6,8 @@ import open from "open";
 import { authStore } from "../stores/auth.js";
 import { configStore } from "../stores/config.js";
 
-import { errorUrl, loginUrl, successUrl } from "./urls.js";
 import { ParamType } from "./types.js";
+import { errorUrl, loginUrl, successUrl } from "./urls.js";
 
 interface waitForAccessToken {
   accessToken: string;
