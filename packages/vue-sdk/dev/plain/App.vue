d03c1ece9cef73818daa043304afb85dfb1b7e9c
Ron Cohen <ron@bucket.co>
2025-06-19 10:51:58 +0200

    lint fix

--- packages/vue-sdk/dev/plain/App.vue
+++ packages/vue-sdk/dev/plain/App.vue
@@ -3,12 +3,12 @@ import { ref } from "vue";
 
 import { BucketProvider } from "../../src";
 
+import Events from "./components/Events.vue";
 import MissingKeyMessage from "./components/MissingKeyMessage.vue";
 import RequestFeedback from "./components/RequestFeedback.vue";
 import Section from "./components/Section.vue";
 import StartHuddleButton from "./components/StartHuddleButton.vue";
 import Track from "./components/Track.vue";
-import Events from "./components/Events.vue";
 
 const user = ref({ id: "123", name: "John Doe" });
 const publishableKey = import.meta.env.VITE_PUBLISHABLE_KEY || "";
