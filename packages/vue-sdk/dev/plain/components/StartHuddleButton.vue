ee2cd2e4e77bce2bcb2d45bf156916f746cf56e2
Ron Cohen <ron@bucket.co>
2025-06-23 12:32:11 +0200

    fix(vue-js): support destructuring properly (#434)

--- packages/vue-sdk/dev/plain/components/StartHuddleButton.vue
+++ packages/vue-sdk/dev/plain/components/StartHuddleButton.vue
@@ -3,26 +3,25 @@ import { useFeature } from "../../../src";
 
 import Section from "./Section.vue";
 
-const huddle = useFeature("huddle");
+const { isLoading, isEnabled, config, requestFeedback, track } =
+  useFeature("huddle");
 </script>
 <template>
   <Section title="Huddle">
     <div style="display: flex; gap: 10px; flex-wrap: wrap">
-      <div>Huddle enabled: {{ huddle.isEnabled }}</div>
-      <div v-if="huddle.isLoading">Loading...</div>
+      <div>Huddle enabled: {{ isEnabled }}</div>
+      <div v-if="isLoading">Loading...</div>
       <div v-else style="display: flex; gap: 10px; flex-wrap: wrap">
         <div>
-          <button @click="huddle.track()">
-            {{
-              huddle.config.payload?.buttonTitle ?? "Start Huddle (track event)"
-            }}
+          <button @click="track()">
+            {{ config?.payload?.buttonTitle ?? "Start Huddle (track event)" }}
           </button>
         </div>
         <div>
           <button
             @click="
               (e) =>
-                huddle.requestFeedback({
+                requestFeedback({
                   title: 'Do you like huddles?',
                 })
             "
